249664eae5c718f804bb27ead54136e068990942
vicente almeida <vicente.cra45@gmail.com>
2024-02-01 20:34:39 +0400

    Pin position

--- src/components/PartnersSection.tsx
+++ src/components/PartnersSection.tsx
@@ -44,7 +44,8 @@ const PartnersMap = (props : Props) => {
                 top: offsetTop + ((pin.y / 100) * height),
               }}
             >
-              <p>{pin.x ?? 0} </p>
+              <p>{pin.x ?? 0 } </p>
+              <p>{height ?? 0 } </p>
               <img className={` ${pin.id === $selectedPartner?.id ? 'pin-selected' : 'pin-image'}`} draggable="false" src={mapPin} alt="" width="32" height="32" onClick={() => handlePinClick(pin)}/>
             </div>
           ))
