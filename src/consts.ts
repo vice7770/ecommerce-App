315592c1ff774572e49cd61c71375696829b2d8f
vicente almeida <vicente.cra45@gmail.com>
2024-02-01 20:09:38 +0400

    Path

--- src/consts.ts
+++ src/consts.ts
@@ -12,7 +12,7 @@ export type Partner = {
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 
-export const SITE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:4321' : "https://fromgeorgia.com/";
+export const SITE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:4321' : "https://ecommerce-app-cb73-2a9vho8jk-vice7770.vercel.app/";
 
 // export const SITE_URL = 'http://localhost:4321';
 
