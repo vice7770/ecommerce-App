a3f32e5bd158043358daf224f9c1d116fe8af797
vicente almeida <vicente.cra45@gmail.com>
2024-02-01 20:13:34 +0400

    Moving const to component

--- src/components/PartnersSection.tsx
+++ src/components/PartnersSection.tsx
@@ -3,9 +3,41 @@ import { useStore } from "@nanostores/react";
 
 import { useIntersectionObserver } from '../lib/Hooks/useIntersectionObserver';
 import { useMeasure } from '../lib/Hooks/useMeasure';
-import { partners, type Partner } from '@/consts';
+// import { partners, type Partner } from '@/consts';
+import { type Partner } from '@/consts';
 import { selectedPartner, setSelectedPartner } from '../stores/partnerSelected';
 // import { CarouselHomePartners } from "./ui/carouselHomePartners";
+
+export const partners : Partner[] = [
+  {
+      id: 0,
+      name: 'Tbilisi Shop',
+      logo: '/images/astro.png',
+      x: 67,
+      y: 53,
+  },
+  {
+      id: 1,
+      name: 'Kakheti Shop',
+      logo: '/images/blog.png',
+      x: 75,
+      y: 45,
+  },
+  {
+      id: 2,
+      name: 'Batumi Shop',
+      logo: '/images/netlify.png',
+      x: 27,
+      y: 49
+  },
+  {
+      id: 3,
+      name: 'Zugdidi Shop',
+      logo: '/images/netlify.png',
+      x: 27,
+      y: 40
+  }
+];
 
 
 const georgiaPartnersImage = "/georgia-map-partners.png";
